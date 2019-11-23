07bccba00b8c6591ed1d71c9c05a744ca1b11c4c
Hypatia13 <ditrich13@gmail.com>
2019-11-23 19:29:10 +0200

    Installing dependencies

--- app/config/_env.php
+++ app/config/_env.php
@@ -4,4 +4,7 @@ define('BASE_PATH', realpath(__DIR__ . '/../../'));
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $dotEnv = Dotenv\Dotenv::create(BASE_PATH);
-$dotEnv->load();
+$dotEnv->load(); 
+
+// overload() -> for Dotenv to overwrite existing environment variables
+// Existing environment variables might be in $_ENV super global array
