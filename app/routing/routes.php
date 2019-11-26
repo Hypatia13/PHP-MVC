1f7191f8f782e82278fc650f8ac23ecd265b1268
Hypatia13 <ditrich13@gmail.com>
2019-11-26 21:17:52 +0200

    Configure altoRouter

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -4,8 +4,11 @@
 
 $router = new AltoRouter();
 
-# Create a map to a specific target URL
-# (HTTP request method used, 'target' route, 'parameters passed', route name)
+//Create a map to a specific target URL -> a homepage
+// (HTTP request method used, 'target' route, 'parameters passed', route name)
+
+// http://altorouter.com/usage/mapping-routes.html
+
 $router->map('GET', '/', '', 'home');
 
 // Match a given URL against stored routes
@@ -19,7 +22,21 @@ if ($match) {
     $index = new App\Controllers\IndexController();
     $index->show();
 } else {
+    //No route was matched ->
     //Check a server protocol and set it to 404
-    header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found');
+
+    //For PHP<5.4
+    // In Chrome Inspector, a response header is "500 Internal Server Error"?
+    //header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found');
+
+    //For PHP>=5.4
+    // In Chrome Inspector, a response header is "404 Not Found", as it should be
+    http_response_code(404);
+
+    //Show a custom 404.php message
+    //include '404_test.php';
+
     echo "Page not found";
 }
+
+//https://thisinterestsme.com/404-not-found-header-php/
