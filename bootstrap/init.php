bb2e09c97979fad7e4d2f80c3ebf00ae80d7c5f8
Hypatia13 <ditrich13@gmail.com>
2019-12-02 22:31:07 +0200

    Configure a custom error handler

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -13,6 +13,18 @@ require_once __DIR__ . '/../app/config/_env.php';
 
 new \App\Classes\Database();
 
+
+//Set up a custom error handler -> Whoops
+
+//set_error_handler requires a valid callback == a callable function
+//Inside an ErrorHandler class call handleErrors()
+
+set_error_handler([new \App\Classes\ErrorHandler(), 'handleErrors']);
+
+//Same as
+/* $errorHandler = new \App\Classes\ErrorHandler();
+set_error_handler([$errorHandler, 'handleErrors']);*/
+
 require_once __DIR__ . '/../app/routing/routes.php';
 
 //$router (an instance of AltoRouter) is passed from routes.php
