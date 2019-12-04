fe0cab632ce0a0d2944d89ec2aefc1226c56489f
Hypatia13 <ditrich13@gmail.com>
2019-12-04 21:05:10 +0200

    Separate an admin sidebar

--- resources/views/admin/layout/base.blade.php
+++ resources/views/admin/layout/base.blade.php
@@ -13,22 +13,7 @@
     <link rel="stylesheet" href="/css/all.css">
 </head>
 <body>
-
-{{--Sidebar--}}
-<div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas>
-
-    <!-- Menu -->
-    <ul class="vertical menu">
-        <li><a href="#">Foundation</a></li>
-        <li><a href="#">Dot</a></li>
-        <li><a href="#">ZURB</a></li>
-        <li><a href="#">Com</a></li>
-        <li><a href="#">Slash</a></li>
-        <li><a href="#">Sites</a></li>
-    </ul>
-
-</div>
-{{--Sidebar ends--}}
+@include('admin.layout.includes.admin-sidebar')
 
 {{--Main content starts--}}
 
