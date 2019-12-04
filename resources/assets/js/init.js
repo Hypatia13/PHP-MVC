a306bef3244adea169c4d481d7be739dddb6a8fa
Hypatia13 <ditrich13@gmail.com>
2019-12-04 20:51:56 +0200

    JS fixes

--- resources/assets/js/init.js
+++ resources/assets/js/init.js
@@ -1,4 +1,6 @@
 //Include js for Zurb foundation
-function f() {
+(function f() {
+    "use strict";
     $(document).foundation();
-}
+})();
+//Adding parantheses around the function and () after it makes it self-invoking
