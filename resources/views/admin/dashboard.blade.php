bb64504713c93437208d21883e46162b38b5c3ea
Hypatia13 <ditrich13@gmail.com>
2019-12-04 18:13:06 +0200

    Dashboard template for admin pages

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,9 @@
-<h1>Dashboard</h1>
+@extends('admin.layout.base')
+@section('title', 'Dashboard')
+
+@section('content')
+
+    <div class="row expanded">
+        <h2>Dashboard</h2>
+    </div>
+@endsection
