198fefad39e6c8e4d2e14d5a7e04444274682b9d
Hypatia13 <ditrich13@gmail.com>
2019-12-13 20:27:18 +0200

    UploadFile Class addition

--- app/classes/UploadFile.php
+++ app/classes/UploadFile.php
@@ -12,7 +12,7 @@ namespace App\Classes;
 class UploadFile
 {
     protected $filename;
-    protected $max_filesize = 209571;
+    protected $max_filesize = 2097152;
     protected $extension;
     protected $path;
 
@@ -27,11 +27,11 @@ class UploadFile
         if ($name == "") {
             $name = pathinfo($file, PATHINFO_FILENAME);
         }
-        $name = strtolower(str_replace(['_', ' '], '-', $file));
+        $name = strtolower(str_replace(['_', ' '], '-', $name));
 
         //Gives random strings
         $hash = md5(microtime());
-        $ext = $this->fileExtension();
+        $ext = $this->fileExtension($file);
         $this->filename = "{$name}-{$hash}.{$ext}";
     }
 
@@ -43,5 +43,39 @@ class UploadFile
     public function fileSize($file)
     {
         return $file > $this->max_filesize ? true : false;
+    }
+
+    public function isImage($file)
+    {
+        $ext = $this->fileExtension($file);
+        $validExtension = array('jpg', 'jpeg', 'bmp', 'png', 'gif');
+        if (!in_array(strtolower($ext), $validExtension)) {
+            return false;
+        }
+        return true;
+    }
+
+    public function path()
+    {
+        return $this->path;
+    }
+
+    public function move($temp_folder, $folder, $file, $new_filename)
+    {
+        $ds = DIRECTORY_SEPARATOR; // A slash
+
+        $this->setName($file, $new_filename);
+        $file_name = $this->getName();
+
+        if (!is_dir($folder)) {
+            mkdir($folder, 0777, true);
+        }
+
+        $this->path = "{$folder}{$ds}{$file_name}";
+        $absolute_path = BASE_PATH . "{$ds}public{$ds}$this->path";
+        if (move_uploaded_file($temp_folder, $absolute_path)) {
+            return $this; //Return an object to use method chaining
+        }
+        return null;
     }
 }
