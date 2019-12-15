3a3cfedb7485b0578f00134c404b134fae6e55de
Hypatia13 <ditrich13@gmail.com>
2019-12-15 22:59:29 +0200

    Dashboard view test

--- app/controllers/admin/DashboardController.php
+++ app/controllers/admin/DashboardController.php
@@ -8,12 +8,31 @@
 
 namespace App\Controllers\Admin;
 
+use App\Classes\Request;
+use App\Classes\Session;
 use App\Controllers\BaseController;
 
 class DashboardController extends BaseController
 {
     public function show()
     {
-        return view('admin/dashboard');
+        Session::add('crowley', 'Welcome, Crowley!');
+
+        if (Session::has('crowley')) {
+            $msg = Session::get('crowley');
+        } else {
+            $msg = "Not defined";
+        }
+
+        //Pass $data array to a view and array keys are available as variables
+        return view('admin/dashboard', ['crowley' => $msg]);
+
+    }
+
+    public function get()
+    {
+        $result = Request::all();
+        var_dump($result);
+
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,20 @@
 
 @section('content')
 
-    <div class="row expanded">
-        <h2>Dashboard</h2>
+    <div>
+        <div class="row expanded">
+            <h2>Dashboard</h2>
+           {{-- {{\App\Classes\CSRFToken::_token()}}
+            <br>
+            {{\App\Classes\Session::get('token')}}
+            {{$_SERVER['REQUEST_URI']}}--}}
+
+            <form action="/admin" method="post" enctype="multipart/form-data">
+                <input type="text" name="product">
+                <input type="file" name="image">
+                <input type="submit" value="Send">
+            </form>
+
+        </div>
     </div>
 @endsection
